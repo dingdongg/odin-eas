4fc0047d7d896d9ab4dcbfd061f7bf10b732055c
donggyu <donggyu258@gmail.com>
2022-04-11 16:58:27 -0700

    change color buttons dynamically

--- script.js
+++ script.js
@@ -4,15 +4,12 @@
  */
 
 // global variables 
-const RESET_BUTTON = document.createElement('button');
-const RANDOM_COLOR_TOGGLE_BUTTON = document.createElement('button');
-const BRUSH_COLOR_SELECTOR = document.createElement('input');
-const BACK_COLOR_SELECTOR = document.createElement('input');
-const RESOLUTION_SLIDER = document.createElement('input');
+const RANDOM_COLOR_TOGGLE_BUTTON = document.querySelector('.random-color-button');
+const BRUSH_COLOR_SELECTOR = document.querySelector("#brush-color");
+const BUCKET_COLOR_SELECTOR = document.querySelector("#bucket-color");
+const RESOLUTION_SLIDER = document.querySelector(".slider");
 
-const BUTTONS_CONTAINER = document.createElement('div');
 const GRID_CONTAINER = document.querySelector('.grid-container');
-const BODY = document.querySelector('body');
 const DEFAULT_GRID_LENGTH = 16;
 let gridLength = DEFAULT_GRID_LENGTH;
 let randomColorMode = false;
@@ -54,32 +51,14 @@ function resetCells() {
     }
 }
 
-function getUserInput() {
-    let parsedInput = DEFAULT_GRID_LENGTH;
-    do {
-        let userInput = prompt("Enter a grid size (from 1 to 100, inclusive!)");
-        parsedInput = parseInt(userInput);
-    } while (!parsedInput || parsedInput < 1 || parsedInput > 100); // beyond 100 grid length, performance issues spike up
-    return parsedInput;
-}
-
 function configureGrid() {
     resetCells();
-    // gridLength = getUserInput();
     GRID_CONTAINER.setAttribute("style", `grid-template-columns: repeat(${gridLength}, 1fr);`);
     let numCells = gridLength * gridLength;
     createCells(numCells);
 }
 
-function setUpResetBtn() {
-    RESET_BUTTON.classList.add('reset-button');
-    RESET_BUTTON.textContent = 'Reset grid';
-    RESET_BUTTON.addEventListener('click', configureGrid);
-}
-
 function setUpRanColorBtn() {
-    RANDOM_COLOR_TOGGLE_BUTTON.classList.add('random-color-button');
-    RANDOM_COLOR_TOGGLE_BUTTON.textContent = 'Random color!';
     RANDOM_COLOR_TOGGLE_BUTTON.addEventListener('click', toggleRandomColor);
 }
 
@@ -90,62 +69,28 @@ function updateButtonColor() {
 
 function setUpSelectors() {
     // set up brush selector
-    BRUSH_COLOR_SELECTOR.type = 'color';
-    BRUSH_COLOR_SELECTOR.id = 'brush-color';
     BRUSH_COLOR_SELECTOR.value = '#000000';
-    BRUSH_COLOR_SELECTOR.addEventListener('change', updateButtonColor);
+    BRUSH_COLOR_SELECTOR.addEventListener('input', updateButtonColor);
     
-    BACK_COLOR_SELECTOR.type = 'color';
-    BACK_COLOR_SELECTOR.id = 'bucket-color';
-    BACK_COLOR_SELECTOR.value = '#ffffff';
-    BACK_COLOR_SELECTOR.addEventListener('change', updateButtonColor);
-    // set up background color selector
+    BUCKET_COLOR_SELECTOR.value = '#ffffff';
+    BUCKET_COLOR_SELECTOR.addEventListener('input', updateButtonColor);
+    // set up bucket color selector
 }
 
 function applyBucket() {
     let cells = document.querySelectorAll('.row-cell');
-    cells.forEach(cell => cell.setAttribute("style", `background-color: ${BACK_COLOR_SELECTOR.value};`));
+    cells.forEach(cell => cell.setAttribute("style", `background-color: ${BUCKET_COLOR_SELECTOR.value};`));
 }
 
 function appendColorLabels() {
-    let brushLabel = document.createElement('label');
-    brushLabel.textContent = 'Brush Color';
-    brushLabel.for = `${BRUSH_COLOR_SELECTOR.id}`;
-
-    let backgroundLabel = document.createElement('label');
-    backgroundLabel.textContent = 'Bucket Color';
-    backgroundLabel.for = `${BACK_COLOR_SELECTOR.id}`;
-
-    let brushContainer = document.createElement('div');
-    brushContainer.classList.add('color-container');
-    brushContainer.classList.add('brush-color');
+    let brushContainer = BRUSH_COLOR_SELECTOR.parentElement;
     brushContainer.setAttribute("style", `background-color: ${BRUSH_COLOR_SELECTOR.value};`);
-    brushContainer.appendChild(BRUSH_COLOR_SELECTOR);
-    brushContainer.appendChild(brushLabel);
-    BUTTONS_CONTAINER.appendChild(brushContainer);
 
-    let bucketButton = document.createElement('button');
-    bucketButton.classList.add('bucket-button');
-    bucketButton.textContent = "Bucket";
+    let bucketButton = document.querySelector('.bucket-button');
     bucketButton.addEventListener('click', applyBucket);
 
-    let backgroundContainer = document.createElement('div');
-    backgroundContainer.classList.add('color-container');
-    backgroundContainer.classList.add('bucket-color');
-    backgroundContainer.setAttribute("style", `background-color: ${BACK_COLOR_SELECTOR.value};`);
-    backgroundContainer.appendChild(BACK_COLOR_SELECTOR);
-    backgroundContainer.appendChild(bucketButton);
-    backgroundContainer.appendChild(backgroundLabel);
-    BUTTONS_CONTAINER.appendChild(backgroundContainer);
-}
-
-function setUpBtnContainer() {
-    // add all buttons to this container
-    BUTTONS_CONTAINER.classList.add('buttons-container');
-    appendColorLabels();
-    BUTTONS_CONTAINER.appendChild(RANDOM_COLOR_TOGGLE_BUTTON);
-    // BUTTONS_CONTAINER.appendChild(RESET_BUTTON);
-    BUTTONS_CONTAINER.appendChild(RESOLUTION_SLIDER);
+    let bucketContainer = BUCKET_COLOR_SELECTOR.parentElement;
+    bucketContainer.setAttribute("style", `background-color: ${BUCKET_COLOR_SELECTOR.value};`);
 }
 
 function updateResolution() {
@@ -154,26 +99,15 @@ function updateResolution() {
 }
 
 function setUpResSlider() {
-    let sliderDiv = document.createElement('div');
-    sliderDiv.classList.add('slider-container');
-
-    RESOLUTION_SLIDER.type = 'range';
-    RESOLUTION_SLIDER.min = '1';
-    RESOLUTION_SLIDER.max = '100';
     RESOLUTION_SLIDER.value = `${DEFAULT_GRID_LENGTH}`;
-    RESOLUTION_SLIDER.classList.add('slider');
     RESOLUTION_SLIDER.addEventListener('change', updateResolution);
-
-    sliderDiv.appendChild(RESOLUTION_SLIDER);
 }
 
 function initPage() {
-    // setUpResetBtn();
-    setUpResSlider();
-    setUpRanColorBtn();
     setUpSelectors();
-    setUpBtnContainer();
-    BODY.insertBefore(BUTTONS_CONTAINER, GRID_CONTAINER);
+    appendColorLabels();
+    setUpRanColorBtn();
+    setUpResSlider();
     configureGrid();
 }
 
